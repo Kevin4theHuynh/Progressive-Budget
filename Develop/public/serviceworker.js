b188df07ca3bbfa84e8f8c21968d99533d06ebff
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-01 14:45:40 -0800

    More service workers

--- Develop/public/serviceworker.js
+++ Develop/public/serviceworker.js
@@ -1,1 +1,14 @@
 console.log("This is service worker")
+
+const cacheName = 'js13kPWA-v1';
+const  cacheFiles = [
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/transaction.js',
+    '/api.js',
+]
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+  });
