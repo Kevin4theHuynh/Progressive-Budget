56092e80cb1567039bc81c947ccdd360f0921459
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-01 14:58:16 -0800

    More Service

--- Develop/public/serviceworker.js
+++ Develop/public/serviceworker.js
@@ -1,6 +1,8 @@
 console.log("This is service worker")
 
+
 const cacheName = 'js13kPWA-v1';
+// varible for storing all the files 
 const  cacheFiles = [
     '/index.html',
     '/index.js',
@@ -9,6 +11,15 @@ const  cacheFiles = [
     '/api.js',
 ]
 
+// Installer event
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
+    
   });
+
+
+const budgetImages = [];
+    for (let i = 0; i < budget.length; i++) {
+  budgetImages.push(`data/img/${budget[i].slug}.jpg`);
+}
+const cacheContent = cacheFiles.concat(budgetImages);
