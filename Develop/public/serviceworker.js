f60ca6b9cabdce1f1f51db09fec19e0fe4b3ad09
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-06 14:00:35 -0800

    Service worker not working

--- Develop/public/serviceworker.js
+++ Develop/public/serviceworker.js
@@ -1,29 +1,63 @@
-console.log("This is service worker")
 
-
-const cacheName = 'js13kPWA-v1';
+const PRECACHE = 'js13kPWA-v1';
+const RUNTIME = 'runtime';
 // varible for storing all the files 
-const cacheFiles = [
-    '/index.html',
-    '/index.js',
-    '/styles.css',
-    '/transaction.js',
-    '/api.js',
+const FILES_TO_CACHE = [
+    '/',
+    '/public/index.html',
+    '/public/index.js',
+    '/public/styles.css',
+    '/models/transaction.js',
+    '/routes/api.js',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ]
 
 // Installer event
 self.addEventListener('install', (e) => {
     e.waitUntil(
       caches
-      .open(cacheName)
-      .then((cache) => cache.addAll(cacheFiles))
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
       .then(self.skipWaiting())
     )
 })
 
-const budgetImages = [];
-    for (let i = 0; i < budget.length; i++) {
-  budgetImages.push(`data/img/${budget[i].slug}.jpg`);
-}
-const cacheContent = cacheFiles.concat(budgetImages);
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
 
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
