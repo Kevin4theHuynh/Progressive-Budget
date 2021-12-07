598427684da39f54770f77ba59e30940fdbade59
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-12-06 20:41:31 -0800

    Offline works

--- Develop/public/serviceworker.js
+++ Develop/public/serviceworker.js
@@ -55,4 +55,17 @@ self.addEventListener('fetch', (event) => {
     )
     return;
 }
+
+event.respondWith(
+  caches.match(event.request).then(cachedResponse => {
+    if (cachedResponse) {
+      return cachedResponse
+    }
+    return fetch(event.request).then(response => {
+      return cache.put(event.request, response.clone()).then(() => {
+        return response
+      })
+    })
+  })
+)
 })
